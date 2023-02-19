628aaadcf8b5b726730c95ab167406387e41c741
CesarMarro <cemarroquin@ufm.edu>
2023-02-18 20:08:13 -0600

    First draft done
    The program can  now check if an IP is private and what class it is.

---
+++ IpAdress.py
@@ -0,0 +1,41 @@
+def check_if_private(ip_address: str) -> bool:
+
+    ip_parts = ip_address.split(".")
+    first_number = int(ip_parts[0])
+
+    if first_number == 10 or (first_number == 192 and int(ip_parts[1]) == 168) or \
+            (first_number == 172 and 16 <= int(ip_parts[1]) <= 31):
+        return True
+
+def check_class(ip_address: str) -> str:
+    
+    ip_parts = ip_address.split(".")
+    first_number = int(ip_parts[0])
+   
+    if first_number < 128:
+        return "A"
+
+    elif first_number < 192:
+        return "B"
+
+    elif first_number < 224:
+        return "C"
+
+    elif first_number < 240:
+        return "D"
+    
+    elif first_number < 254:
+        return "E"
+
+
+
+def check_ip(ip_address:str) -> str: 
+    is_private = check_if_private(ip_address)
+    ip_class = check_class(ip_address)
+    
+    if is_private == True:
+        str_is_private = "privada"
+    else:
+        str_is_private = "publica"
+
+    return "La IP %s es %s y es de clase %s" %(ip_address, str_is_private, ip_class)
---
+++ main.py
@@ -0,0 +1,4 @@
+from IpAdress import check_ip
+
+ip_adress = str(input("Ingrese su direccion IP: "))
+print(check_ip(ip_adress))
